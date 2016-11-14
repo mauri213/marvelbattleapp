56c149c8aca5490862fce63c8e9c9f7776355150
Mauricio Orozco <Mauricio Orozco>
2016-11-14 14:16:58 -0500

    Updated notes

--- messages-ideas.js
+++ messages-ideas.js
@@ -21,7 +21,7 @@ Thrown off balance, Iron Patriot (James Rhodes) is unable to land a blow to, Iro
 
 Iron Man slips on the uneven terrain, 
 shooting wide and missing Iron Patriot (James Rhodes)
-"Iron Man" is distracted by a taco truck driving by the jump kick misses "Iron Patriot (James Rhodes)"
+"Iron Man" is distracted by a taco truck driving by thus the jump kick misses "Iron Patriot (James Rhodes)"
 
 Both fighters step back, breathing heavily. 
 'You can't win', says Iron Man. 
@@ -36,4 +36,60 @@ coming just in time to block it and dodge away.
 The sun is in Iron Patriot (James Rhodes)'s eyes. 
 Squinting in the light, Iron Patriot (James Rhodes) fires. 
 Missing Iron Man by a wide margin.
+"Iron Patriot" tries to use targeting computer to land some damage.
+"Iron Patriot (James Rhodes)" misses "Iron Man" badly because he didn't use the Force.
 
+Off balance, Iron Patriot (James Rhodes) attacks at range, 
+missing Iron Man as the fighter stumbles over debree.
+"Iron Patriot (James Rhodes)" launches a stink bomb at "Iron Man"
+however the stink bomb is a dud and causes no damage.
+
+As Iron Patriot (James Rhodes) steps forward to assault, 
+Iron Man steps forward to block, 
+anticipating the attack and stopping it before its begun.
+"Iron Patriot (James Rhodes)" chooses rock.
+"Iron Man" chooses rock as well. Both combantants roll their eyes at each other.
+
+Jumpy with anticipation, 
+Iron Man fires from a distance, 
+narrowly missing Iron Patriot (James Rhodes)
+"Iron Man" takes a swig of Mountain Dew but his left jab
+narrowly misses "Iron Patriot (James Rhodes)".
+
+Iron Man stands firm, blocking all attacks byIron Patriot (James Rhodes).
+"Iron Man" obtains Super Star and is invincible to all of 
+"Iron Patriot (James Rhodes)"'s attacks.
+
+Furious and fumbling May Parker dives in to assault Thanos clumsily missing.
+Trying to text and fight at the same time "May Parker" attacks blindly and misses "Thanos".
+
+Faster than Thanos can react, May Parker fires a ranged attack that cannot be dodged.
+Faster than a speeding bullet, "May Paker" unloads a Stephen Seagal-esc combo on "Thanos"
+
+Dodging into close range, Thanos lands a blow, knocking May Parker to the ground.
+"Thanos" takes Kreese's advice and sweeps the leg. "May Parker" promptly falls
+to the ground in pain.
+
+Thanos peels off an attack from a distance, narrowly missing May Parker
+"Thanos" launches a water balloon from 40 meters, narrowly missing "May Parker"
+
+Thanos smashes into May Parker
+"Thanos" lands a Shroyuken into "May Parker"
+
+Thanos throws May Parker's limp body into the street, 
+where it tumbles to a stop in the gutter. 
+'You asked for this...' Thanos says. 
+May Parker groans in pain. Thanos wins!
+"Thanos" calls an Uber to have "May Parker" taken to the nearest hospital.
+"Thanos" WINS!!!
+
+Shielded by the fallen debris of a nearby, 
+demolished building, Shadow King blocks Songbird's attack.
+Shielded by a mother in law, "Shadow King" blocks "Songbird"'s attack.
+
+'No more...' says Songbird 'You win.' 
+Shadow King gazes down at Songbird, pitiless and victorious. 
+Shadow King wins!
+'No mas!' screams "Songbird" 'A winner is you.'
+"Shadow King" chuckles and says 'All your base are belong to us'
+"Shadow King" WINS!!!
